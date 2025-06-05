30cb2ee943962f6f912db188c836707d1fc8eabf
Andres Cochez <73442471+AndresCochez@users.noreply.github.com>
2025-06-05 20:52:42 +0200

    Delete BreezdPage-main directory

--- BreezdPage-main/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();     // Verwijder alle sessievariabelen
-session_destroy();   // Vernietig de sessie
-
-header("Location: ../index.php"); // Terug naar de startpagina
-exit();
-?>
